249be33cfbaed672339978b0ec1e4208e24b5d06
Ngawang Tashi Moktan <45892775+ngawang-tashi1986@users.noreply.github.com>
2025-06-27 00:59:30 +0800

    Specify Node.js version 20.x in package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ app.use(
 );
 
 app.use(express.json()); // to parse req.body
-app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
+//app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
 
 //app.use(clerkMiddleware({}));
 
