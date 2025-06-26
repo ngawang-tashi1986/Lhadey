e19fa25d967d06b702587bb2a51c678701d8244c
Ngawang Tashi Moktan <45892775+ngawang-tashi1986@users.noreply.github.com>
2025-06-26 23:20:52 +0800

    change in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,11 @@ app.use(
 );
 
 app.use(express.json()); // to parse req.body
-app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
+//app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
+
+app.use("/api", userRoutes, adminRoutes, authRoutes, songRoutes, albumRoutes, statRoutes);
+app.use(clerkMiddleware({}));
+
 app.use(
 	fileUpload({
 		useTempFiles: true,
